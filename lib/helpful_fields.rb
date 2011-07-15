fee6457a128756785f726077b25537e91d5b552a
grosser <grosser.michael@gmail.com>
2011-07-15 21:55:06 +0200

    move stuff araound

--- lib/helpful_fields.rb
+++ lib/helpful_fields.rb
@@ -13,6 +13,7 @@ class HelpfulFields
   end
 
   module TagHelper
+    # --- misc
     def params_text_field_tag(name, options={})
       text_field_tag name, params.value_from_nested_key(name), options
     end
@@ -25,20 +26,13 @@ class HelpfulFields
       hidden_field_tag name, params.value_from_nested_key(name), options
     end
 
-    def radio_button_with_label(name, value, checked, label, options={})
-      label_for = if options[:id]
-        options[:id]  # when id was changed, label has to be for this id
-      else
-        # name[hello][world] -> name_hello_world_
-        # name[hello][] -> name_hello_world__
-        # name -> name_
-        clean_name = name.to_s.gsub('[]','_').gsub('][','_').gsub(/[\]\[]/,'_')
-        clean_name += '_' unless clean_name =~ /_$/
-        clean_name + value.to_s.downcase
-      end
-      radio_button_tag(name, value, checked) + label_tag(label_for, label)
+    def select_options_tag(name, list, options={})
+      list = list.map{|x| x.is_a?(Array) ? [x[0],h(x[1])] : h(x) } # stringify values from lists
+      selected = h(options[:value] || params.value_from_nested_key(name))
+      select_tag(name, options_for_select(list,selected), options.except(:value))
     end
 
+    # --- check_box
     def params_check_box_tag(name, value, options={})
       check_box_tag(name, value, HelpfulFields.check_box_checked?(params, name, value), options)
     end
@@ -52,10 +46,19 @@ class HelpfulFields
       check_box_with_label(name, value, HelpfulFields.check_box_checked?(params, name, value), label, options)
     end
 
-    def select_options_tag(name, list, options={})
-      list = list.map{|x| x.is_a?(Array) ? [x[0],h(x[1])] : h(x) } # stringify values from lists
-      selected = h(options[:value] || params.value_from_nested_key(name))
-      select_tag(name, options_for_select(list,selected), options.except(:value))
+    # --- radio_button
+    def radio_button_with_label(name, value, checked, label, options={})
+      label_for = if options[:id]
+        options[:id]  # when id was changed, label has to be for this id
+      else
+        # name[hello][world] -> name_hello_world_
+        # name[hello][] -> name_hello_world__
+        # name -> name_
+        clean_name = name.to_s.gsub('[]','_').gsub('][','_').gsub(/[\]\[]/,'_')
+        clean_name += '_' unless clean_name =~ /_$/
+        clean_name + value.to_s.downcase
+      end
+      radio_button_tag(name, value, checked) + label_tag(label_for, label)
     end
   end
 
